9040fb9e7ac294066b02dc72386e50b22fccd4c5
Jonathan <new.jonathan01@gmail.com>
2020-08-31 06:52:17 -0300

    Front do Resultado alterada

--- leitor/src/main/java/com/gobots/leitor/LeitorController.java
+++ leitor/src/main/java/com/gobots/leitor/LeitorController.java
@@ -33,41 +33,56 @@ public class LeitorController {
 
             textoRepository.save(u);
             
-            return  
+            return             
             "<head>"+
-            "<meta charset=\"UTF-8\">"+
-            "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">"+
-
-            "<title>Olá, mundo!</title>"+
+                "<meta charset=\"UTF-8\">"+
+                "<link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">"+
+                "<link rel=\"stylesheet\" href=\"/css/leitor.css\">"+
+                "<link href=\"https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap\" rel=\"stylesheet\">"+
+                "<title>Leitor</title>"+
+                "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"+
             "</head>"+
-            "<body>" +
-            "<h1>Olá, mundo!</h1>"+
-        
-        
-            "<div class=\"row\">"+
-            
-              "<div class=\"col-sm\">"+
-              "</div>"+
 
-              "<div class=\"col-sm-3\">"+
-                u.getTexto()+
-                "<a href=\"/leitor\">Inicio</a>"+
-              "</div>"+
-              
-              "<div class=\"col-sm\">"+
-              "</div>"+
-            
+            "<body style=\"background-color:white;\">"+
+
+
+            "<div class=\"row menu\">"+
+                "<div class=\"col-sm \">"+
+                    "<p style=\"margin-top: 7.5px;text-align: right;margin-right: 7.5px;\"><a style=\"color: white;\" href=\"https://gobots.ai/\">GoBots</a></p>"+
+                "</div>"+
             "</div>"+
-          
-            
-          
-            "<script type=\"text/javascript\">" +
-          
-              "var msg = new SpeechSynthesisUtterance(\"" + u.getTexto() + "\");" +
-              "window.speechSynthesis.speak(msg);" +
+
+
+            "<div class=\"row\">"+
+
+                "<div class=\"col-sm img\">"+
+                    "<img src=\"/img/gobots.png\">"+
+                "</div>"+
+                
+                "<div class=\"col-sm formulario\">"+
+                    "<p style=\"color: #1f2124; font-size: x-large; padding-top: 8px;\">Texto Convertido</p>"+
+                    u.getTexto()+
+                    "<div>"+
+                    "<br>"+
+                    "<br>"+
+                    "<a style=\"background-color: #6EC1E4;"+
+                    "color: white;"+
+                    "border: 2px solid; \"href=\"/leitor\">Voltar ao Conversor</a>"+
+                    "</div>"+
+                "</div>"+
+
+            "</div>"+
+
+            "<script type=\"text/javascript\">"+
+                "var msg = new SpeechSynthesisUtterance(\"" + u.getTexto() + "\");"+
+                "window.speechSynthesis.speak(msg);"+
+
             "</script>"+
+            "<script src=\"https://code.jquery.com/jquery-3.3.1.slim.min.js\" integrity=\"sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo\" crossorigin=\"anonymous\"></script>"+
+            "<script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js\" integrity=\"sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49\" crossorigin=\"anonymous\"></script>"+
             "<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script>"+
             "</body>"
+
             ;
     }
 }
